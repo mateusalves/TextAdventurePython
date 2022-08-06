79e69673634829e5149b9aca9cef9fc2969ffe8b
Mateus Alves <mateus.alves.unb@gmail.com>
2022-08-06 10:50:09 +0200

    Creating method to determine the winner. Also, creating algorithm to generate boss strategy. Finally, adjusting screens to show who won.

--- src/lord_of_war.py
+++ src/lord_of_war.py
@@ -52,25 +52,67 @@ class LordOfWar():
                    \              < < <       |        *    |    |  (  \_/  )  |    |    *     /____________________\\
                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~         *|*  *|*  \_-+-_/  *|*  *|*          \+__+__+__+__+__+__*/\n\n''')
         if not winner:
-            print('Boss strategy:\t\t\t%s\t\t\t\t%s\t\t\t\t%s' %
+            print('Boss strategy:\t\t\t%s\t\t\t\t\t%s\t\t\t\t%s' %
                   (str(self.boss_strategy['navy']).zfill(2) if review_data else '**',
                    str(self.boss_strategy['airforce']).zfill(2) if review_data else '**',
                    str(self.boss_strategy['army']).zfill(2) if review_data else '**'))
-            print('Player strategy:\t%s\t\t\t\t%s\t\t\t\t%s' %
+            print('Player strategy:\t\t%s\t\t\t\t\t%s\t\t\t\t%s' %
                   (str(self.player_strategy['navy']).zfill(2) if review_data else '**',
                    str(self.player_strategy['airforce']).zfill(2) if review_data else '**',
                    str(self.player_strategy['army']).zfill(2) if review_data else '**'))
 
             if(review_data):
                 sleep(3)
-                print('\t\t     %s Won\t\t\t     %s Won\t\t\t     %s Won' %
-                      ('Player' if (0) else 'Boss',
-                      'Player' if (1) else 'Boss',
-                      'Player' if (1) else 'Boss'))
+                print('\t\t\t     %s\t\t\t\t     %s\t\t\t     %s' %
+                      (self.category_analyzer('navy'),
+                       self.category_analyzer('airforce'),
+                       self.category_analyzer('army')))
                 sleep(3)
-                print('')
+                round_winner = self.round_analyzer()
+                if round_winner == 'Draw':
+                    print('\n\n\t\t\t\t\t\t\t\t     **Draw game.**')
+                    print('This round will not be counted.')
+                else:
+                    print(f'\n\n\t\t\t\t\t\t\t     **{round_winner} won this round.**')
+                sleep(2)
         else:
-            pass
+            print(f'\t\t\t\t\t\t\t    {winner} is the Lord of War.')
+
+    def category_analyzer(self, category):
+        if self.player_strategy[category] > self.boss_strategy[category]:
+            category_winner = 'Player won'
+        elif self.player_strategy[category] < self.boss_strategy[category]:
+            category_winner = 'Boss won'
+        else:
+            category_winner = 'Draw game'
+        return category_winner
+
+    def round_analyzer(self):
+        round_winner = ''
+        player_round_victories = 0
+        boss_round_victories = 0
+        categories = ['navy', 'airforce', 'army']
+
+        for category in categories:
+            category_winner = self.category_analyzer(category)
+            if category_winner == 'Player won':
+                player_round_victories += 1
+            elif category_winner == 'Boss won':
+                boss_round_victories += 1
+            else:
+                #draw does not compute
+                pass
+
+        if player_round_victories > boss_round_victories:
+            round_winner = 'Player'
+            self.player_victories += 1
+        elif player_round_victories < boss_round_victories:
+            round_winner = 'Boss'
+            self.boss_victories += 1
+        else:
+            round_winner = 'Draw'
+
+        return round_winner
 
     def reset_game(self):
         self.boss_victories = 0
@@ -79,11 +121,10 @@ class LordOfWar():
         self.player_strategy = {'navy': 0, 'airforce': 0, 'army': 0}
 
     def boss_strategy_generator(self):
-        guess = choice(string.ascii_letters)
-        # guess = choice(self.secret_word) #op boss
-        if guess in self.chosen_letters:
-            return self.boss_guess()
-        return guess
+        navy = randint(0, 100)
+        airforce = randint(0, 100 - navy)
+        army = 100 - navy - airforce
+        self.boss_strategy = {'navy': navy, 'airforce': airforce, 'army': army}
 
     def finished(self):
         if self.boss_victories >= 2 or self.player_victories >= 2:
@@ -95,3 +136,4 @@ class LordOfWar():
 if __name__ == "__main__":
     war_game = LordOfWar()
     war_game.play()
+
