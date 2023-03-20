ec4092f2f5337cb955f4129a3aebb583ded1b87b
Mateus Alves <mateus.alves.unb@gmail.com>
2023-03-20 20:53:46 +0100

    Adjusting with linter

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -44,16 +44,16 @@ class TicTacToe(Challenge):
                 3  %s  |  %s  |  %s
                       |     |     \n\n""" %
               (self.moves['1a'], self.moves['1b'], self.moves['1c'],
-              self.moves['2a'], self.moves['2b'], self.moves['2c'],
-              self.moves['3a'], self.moves['3b'], self.moves['3c']))
+               self.moves['2a'], self.moves['2b'], self.moves['2c'],
+               self.moves['3a'], self.moves['3b'], self.moves['3c']))
 
     def reset_game(self):
         self.moves = {'1a': '-', '1b': '-', '1c': '-',
                       '2a': '-', '2b': '-', '2c': '-',
                       '3a': '-', '3b': '-', '3c': '-'}
         self.moves_left = ['1a', '1b', '1c',
-                          '2a', '2b', '2c',
-                          '3a', '3b', '3c']
+                           '2a', '2b', '2c',
+                           '3a', '3b', '3c']
         self.champz = None
 
     def next_movement(self, player_movement):
@@ -80,38 +80,46 @@ class TicTacToe(Challenge):
             print("Choose line and column in this order. Ex: '1a'")
             input('Press any key to continue...')
 
-
     def finished(self):
-        if(self.moves['1a'] != '-' and self.moves['1a'] == self.moves['1b'] and
-           self.moves['1a'] == self.moves['1c']):
+        if (self.moves['1a'] != '-' and
+                self.moves['1a'] == self.moves['1b'] and
+                self.moves['1a'] == self.moves['1c']):
             return self.moves['1a']
-        elif(self.moves['2a'] != '-' and self.moves['2a'] == self.moves['2b'] and
-             self.moves['2a'] == self.moves['2c']):
+        elif (self.moves['2a'] != '-' and
+              self.moves['2a'] == self.moves['2b'] and
+              self.moves['2a'] == self.moves['2c']):
             return self.moves['2a']
-        elif(self.moves['3a'] != '-' and self.moves['3a'] == self.moves['3b'] and
-             self.moves['3a'] == self.moves['3c']):
+        elif (self.moves['3a'] != '-' and
+              self.moves['3a'] == self.moves['3b'] and
+              self.moves['3a'] == self.moves['3c']):
             return self.moves['3a']
 
-        elif(self.moves['1a'] != '-' and self.moves['1a'] == self.moves['2a'] and
-           self.moves['1a'] == self.moves['3a']):
+        elif (self.moves['1a'] != '-' and
+              self.moves['1a'] == self.moves['2a'] and
+              self.moves['1a'] == self.moves['3a']):
             return self.moves['1a']
-        elif(self.moves['1b'] != '-' and self.moves['1b'] == self.moves['2b'] and
-             self.moves['1b'] == self.moves['3b']):
+        elif (self.moves['1b'] != '-' and
+              self.moves['1b'] == self.moves['2b'] and
+              self.moves['1b'] == self.moves['3b']):
             return self.moves['1b']
-        elif(self.moves['1c'] != '-' and self.moves['1c'] == self.moves['2c'] and
-             self.moves['1c'] == self.moves['3c']):
+        elif (self.moves['1c'] != '-' and
+              self.moves['1c'] == self.moves['2c'] and
+              self.moves['1c'] == self.moves['3c']):
             return self.moves['1c']
 
-        elif(self.moves['1a'] != '-' and self.moves['1a'] == self.moves['2b'] and
-           self.moves['1a'] == self.moves['3c']):
+        elif (self.moves['1a'] != '-' and
+              self.moves['1a'] == self.moves['2b'] and
+              self.moves['1a'] == self.moves['3c']):
             return self.moves['1a']
-        elif(self.moves['1c'] != '-' and self.moves['1c'] == self.moves['2b'] and
-             self.moves['1c'] == self.moves['3a']):
+        elif (self.moves['1c'] != '-' and
+              self.moves['1c'] == self.moves['2b'] and
+              self.moves['1c'] == self.moves['3a']):
             return self.moves['1c']
         elif not self.moves_left:
             return 'draw'
         else:
             return None
+
 
 if __name__ == "__main__":
     ticTacToe = TicTacToe()
